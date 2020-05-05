51a1ebd3bd716593afa93755a21b795e03807150
Nico Koenig <nico.koenig@fullstax.de>
2020-05-05 11:49:10 +0200

    feat: adds mouse cursor component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { BooksModule } from './books/books.module';
 import { AboutModule } from './about/about.module';
 import { NavigationComponent } from './navigation/navigation.component';
 import { InfoBoxComponent } from './info-box/info-box.component';
+import { MouseCursorComponent } from './mouse-cursor/mouse-cursor.component';
 
 @NgModule({
-  declarations: [AppComponent, NavigationComponent, InfoBoxComponent],
+  declarations: [AppComponent, NavigationComponent, InfoBoxComponent, MouseCursorComponent],
   imports: [BrowserModule, BooksModule, AboutModule],
   providers: [],
   bootstrap: [AppComponent],
