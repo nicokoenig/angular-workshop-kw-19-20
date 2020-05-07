05ee94ec4fef236a4d82db3d3d9732f1f436e9ca
Nico Koenig <nico.koenig@fullstax.de>
2020-05-07 11:38:54 +0200

    feat: use ng for

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { NavigationComponent } from './navigation/navigation.component';
 import { InfoBoxComponent } from './info-box/info-box.component';
 import { MouseCursorComponent } from './mouse-cursor/mouse-cursor.component';
 import { TitleBoxComponent } from './title-box/title-box.component';
+import { BookListComponent } from './book-list/book-list.component';
 
 @NgModule({
-  declarations: [AppComponent, NavigationComponent, InfoBoxComponent, MouseCursorComponent, TitleBoxComponent],
+  declarations: [AppComponent, NavigationComponent, InfoBoxComponent, MouseCursorComponent, TitleBoxComponent, BookListComponent],
   imports: [BrowserModule, BooksModule, AboutModule],
   providers: [],
   bootstrap: [AppComponent],
