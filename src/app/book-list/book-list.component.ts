23a636caa329d2a3dc937d01a56dea4843945832
Nico Koenig <nico.koenig@fullstax.de>
2020-05-12 09:52:27 +0200

    feat: adds async pipe

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,25 +1,17 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Book, BookDataService } from '../books/book-data.service';
-import { Subscription } from 'rxjs';
+import { Subscription, Observable } from 'rxjs';
 @Component({
   selector: 'ba-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.scss'],
 })
-export class BookListComponent implements OnInit, OnDestroy {
-  books: Book[] = [];
+export class BookListComponent {
+  books$: Observable<Book[]>;
 
   subscription: Subscription = Subscription.EMPTY;
 
-  constructor(public bookData: BookDataService) {}
-
-  ngOnInit(): void {
-    this.subscription = this.bookData.getBooks().subscribe((books) => {
-      this.books = books;
-    });
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
+  constructor(public bookData: BookDataService) {
+    this.books$ = this.bookData.getBooks();
   }
 }
