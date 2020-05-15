6f9f3f1252170ce5ae941fa2056161960b77b4a0
Nico Koenig <nico.koenig@fullstax.de>
2020-05-15 10:46:52 +0200

    feat: adds weird stylings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { BookDataService } from './books/book-data.service';
 
 @Component({
   selector: 'ba-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom,
 })
 export class AppComponent {
   color = 'hotpink';
